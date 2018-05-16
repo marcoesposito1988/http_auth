7c0f63394f9c5686ff90012acf5872c6566c30c6
Marco Esposito <marco.esposito@tum.de>
2018-05-16 17:19:11 +0200

    More and cleaner tests

--- test/http_auth_test.dart
+++ test/http_auth_test.dart
@@ -8,45 +8,77 @@ import 'package:test/test.dart';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  group('Basic Auth', () {
+  group('httpbin Basic Auth', () {
     http.BaseClient client;
-    String url = 'http://eu.httpbin.org/basic-auth/user/passwd';
 
 //    setUp(() {
 //
 //    });
 
-    test('One request', () async {
+    test('HTTP', () async {
+      String url = 'http://eu.httpbin.org/basic-auth/user/passwd';
       client = new BasicAuthClient("user", "passwd");
 
       var response = await client.get(url);
       expect(response.statusCode == 200, isTrue);
     });
+
+    test('HTTPS', () async {
+      String url = 'https://eu.httpbin.org/basic-auth/user/passwd';
+      client = new BasicAuthClient("user", "passwd");
+
+      var response = await client.get(url);
+      expect(response.statusCode == 200, isTrue);
+    });
+
   });
 
-  group('Digest Auth', () {
+  group('httpbin Digest Auth', () {
     http.BaseClient client;
-    String url = 'http://eu.httpbin.org/digest-auth/auth/user/passwd';
 
 //    setUp(() {
 //
 //    });
 
-    test('One request', () async {
+    test('HTTP', () async {
+      String url = 'http://eu.httpbin.org/digest-auth/auth/user/passwd';
       client = new DigestAuthClient("user", "passwd");
 
       var response = await client.get(url);
       expect(response.statusCode == 200, isTrue);
     });
 
-    test('Two requests', () async {
+    test('HTTPS', () async {
+      String url = 'https://eu.httpbin.org/digest-auth/auth/user/passwd';
       client = new DigestAuthClient("user", "passwd");
 
       var response = await client.get(url);
       expect(response.statusCode == 200, isTrue);
+    });
 
-      response = await client.get(url);
-      expect(response.statusCode == 200, isTrue);
+  });
+
+  group('jigsaw Digest Auth', () {
+    http.BaseClient client;
+
+//    setUp(() {
+//
+//    });
+
+    test('HTTP', () async {
+      String url = 'http://jigsaw.w3.org/HTTP/Digest/';
+      client = new DigestAuthClient("guest", "guest");
+
+      var response = await client.get(url);
+      expect(response.statusCode, 200);
+    });
+
+    test('HTTPS', () async {
+      String url = 'https://jigsaw.w3.org/HTTP/Digest/';
+      client = new DigestAuthClient("guest", "guest");
+
+      var response = await client.get(url);
+      expect(response.statusCode, 200);
     });
   });
 }
